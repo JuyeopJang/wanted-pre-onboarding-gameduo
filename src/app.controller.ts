d9653cd1c7bbb7bdab67a045b5b3687bd782ca6d
김태용 <amico741@naver.com>
2022-07-11 15:45:36 +0900

    Setting: httpExceptionFilter 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  UseFilters,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(new HttpExceptionFilter())
   getHello(): string {
-    return this.appService.getHello();
+    throw new ForbiddenException();
   }
 }
